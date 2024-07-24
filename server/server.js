3ea83ab63f713a28f4ee745a86722e6cdd373593
Christian Richard Fairhall Smith <fairhallsmith@gmail.com>
2024-07-24 10:29:00 +0100

    Adding clientsides

--- server/server.js
+++ server/server.js
@@ -11,6 +11,17 @@ dotenv.config();
 // CORS is for allowing
 // .env is a file you can put private keys in while including it in the .gitignore
 
-app.listen(8080, function () {
-  console.log(`Mods, crush his skull.`);
+app.get("/", function (request, response) {
+  response.json({
+    message: `"Radical simply means grasping things at the root." - Angela Davis`,
+  });
 });
+app.listen(8080, function () {
+  console.log(`On port eight thousand and eighty.`);
+});
+
+// GET is for getting information from the server
+// POST is when you need to post something to the server
+// PUT is when you need to replace an item
+// PATCH is when you need to modify an item without changing the whole thing with PUT
+// DELETE is the most sensibly named of these
